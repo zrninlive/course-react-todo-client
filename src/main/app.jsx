9b8e2acca692d380cd58ff33aa68cf2fc1c366d5
Christian Oliveira <christian.soliveira@outlook.com>
2018-06-15 13:38:35 -0300

    Refatoring module todo using Redux framework

--- src/main/app.jsx
+++ src/main/app.jsx
@@ -6,6 +6,7 @@ import React from 'react'
 import Menu from '../template/menu'
 import Routes from './routes'
 
+
 export default props => (
     <div className="container">
         <Menu />
---
+++ src/todo/todoActions.js
@@ -0,0 +1,58 @@
+import axios from 'axios'
+
+const URL = 'http://localhost:3003/api/todos'
+
+export const changeDescription = (event) => ({
+    type: 'DESCRIPTION_CHANGED',
+    payload: event.target.value
+})
+
+export const search = (description) => {
+    return (dispatch, getState) => {
+        const description = getState().todo.description
+
+        const filter = description ? `&description__regex=/${description}/` : ''
+
+        const request = axios.get(`${URL}?sort=-createdAt${filter}`)
+            .then(resp => dispatch({ type: 'TODO_SEARCHED', payload: resp.data}))
+    }
+}
+
+export const add = description => {
+    return dispatch => {
+        axios.post(URL, { description })
+            .then(resp => dispatch( clear() ))
+            .then(resp => dispatch( search() ))
+    }
+}
+
+export const markAsDone = (todo) => {
+    return dispatch => {
+        axios.put(`${URL}/${todo._id}`, { ...todo, done: true })
+            .then(resp => dispatch({ type: 'TODO_MARKED_AS_DONE', payload: resp.data }))
+            .then(resp => dispatch( search() ))
+    }
+}
+
+export const markAsPending = (todo) => {
+    return dispatch => {
+        axios.put(`${URL}/${todo._id}`, { ...todo, done: false })
+            .then(resp => dispatch({ type: 'TODO_MARKED_AS_PENDING', payload: resp.data }))
+            .then(resp => dispatch( search() ))
+    }
+}
+
+//Middleware thunk
+export const remove = (todo) => {
+    return dispatch => {
+        axios.delete(`${URL}/${todo._id}`)
+            .then(resp => dispatch( search()))
+    }
+}
+
+//Middleware Multi
+export const clear = () => {
+    return [
+        { type: 'TODO_CLEAR' }, search()
+    ]
+}
